f8f4a980a5d248f76ed023ae707d3c62413ad637
Antonio Etayo <antonioetayo@gmail.com>
2022-08-17 18:51:29 -0400

    Removing pdf buttons

--- scripts/cp-editor.js
+++ scripts/cp-editor.js
@@ -283,10 +283,6 @@ H5PEditor.CoursePresentation.prototype.appendTo = function ($wrapper) {
     .appendTo($settingsWrapper);
 
   // Add and bind slide controls.
-  var pdfInput = H5PEditor.$('<input class="h5p-slidecontrols-pdfinput" type="file" accept=".pdf">');
-  pdfInput.change((event) => { that.processPdf(event, that) });
-  var pdfLabelContainer = H5PEditor.$('<label class="h5p-slidecontrols-button-pdf">PDF</label>');
-  pdfLabelContainer.append(pdfInput);
   var slideControls = {
     $add: H5PEditor.$('<a href="#" aria-label="' + H5PEditor.t('H5PEditor.CoursePresentation', 'newSlide') + '" class="h5p-slidecontrols-button h5p-slidecontrols-button-add"></a>'),
     $clone: H5PEditor.$('<a href="#" aria-label="' + H5PEditor.t('H5PEditor.CoursePresentation', 'cloneSlide') + '" class="h5p-clone-slide h5p-slidecontrols-button h5p-slidecontrols-button-clone"></a>'),
@@ -294,9 +290,6 @@ H5PEditor.CoursePresentation.prototype.appendTo = function ($wrapper) {
     $sortLeft: H5PEditor.$('<a href="#" aria-label="' + H5PEditor.t('H5PEditor.CoursePresentation', 'sortSlide', {':dir': 'left'}) + '" class="h5p-slidecontrols-button h5p-slidecontrols-button-sort-left"></a>'),
     $sortRight: H5PEditor.$('<a href="#" aria-label="' + H5PEditor.t('H5PEditor.CoursePresentation', 'sortSlide', {':dir': 'right'}) + '" class="h5p-slidecontrols-button h5p-slidecontrols-button-sort-right"></a>'),
     $delete: H5PEditor.$('<a href="#" aria-label="' + H5PEditor.t('H5PEditor.CoursePresentation', 'removeSlide') + '" class="h5p-slidecontrols-button h5p-slidecontrols-button-delete"></a>'),
-    //$pdf: H5PEditor.$('<a href="#" aria-label="add from pdf" class="h5p-slidecontrols-button">PDF</a>')
-    // $pdf: H5PEditor.$('<input id="pdfinput" type="file" accept=".pdf">')
-    $pdf: pdfLabelContainer
   };
   this.slideControls = slideControls;
 
@@ -306,8 +299,7 @@ H5PEditor.CoursePresentation.prototype.appendTo = function ($wrapper) {
     slideControls.$background,
     slideControls.$sortLeft,
     slideControls.$sortRight,
-    slideControls.$delete,
-    slideControls.$pdf
+    slideControls.$delete
   ]).appendTo(this.cp.$wrapper)
     .children('a:first')
     .click(function () {
@@ -349,8 +341,6 @@ H5PEditor.CoursePresentation.prototype.appendTo = function ($wrapper) {
       that.removeSlide();
       return false;
     });
-    // .next()
-    // .change((event) => { that.processPdf(event, that) });
 
   if (this.cp.activeSurface) {
     // Enable adjustments
